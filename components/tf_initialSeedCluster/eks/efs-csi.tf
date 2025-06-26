327990a5c46c398a2b652b717d4e437b8d2e7790
cgoolsby <cvioling@gmail.com>
2025-06-26 03:31:43 -0500

    feat: Add AWS EFS CSI driver support
    - Add Terraform IAM resources for EFS CSI driver with IRSA
    - Create Flux Kustomization and HelmRelease for aws-efs-csi-driver
    - Add EFS_CSI_ROLE_ARN to terraform outputs ConfigMap
    - Configure EFS CSI driver with controller and node service accounts
    - Include default EFS storage class configuration
    
    The EFS CSI driver enables dynamic provisioning of Amazon EFS volumes
    for Kubernetes workloads requiring shared persistent storage.

---
+++ components/tf_initialSeedCluster/eks/efs-csi.tf
@@ -0,0 +1,81 @@
+data "aws_iam_policy_document" "efs_csi_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:efs-csi-controller-sa"]
+    }
+
+    principals {
+      identifiers = [module.eks.oidc_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "efs_csi_role" {
+  assume_role_policy = data.aws_iam_policy_document.efs_csi_assume_role.json
+  name               = "AmazonEKS_EFS_CSI_DriverRole"
+}
+
+resource "aws_iam_policy" "efs_csi_policy" {
+  name = "AmazonEKS_EFS_CSI_Driver_Policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticfilesystem:DescribeAccessPoints",
+          "elasticfilesystem:DescribeFileSystems",
+          "elasticfilesystem:DescribeMountTargets",
+          "ec2:DescribeAvailabilityZones"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticfilesystem:CreateAccessPoint"
+        ]
+        Resource = "*"
+        Condition = {
+          StringLike = {
+            "aws:RequestTag/efs.csi.aws.com/cluster" = "true"
+          }
+        }
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticfilesystem:TagResource"
+        ]
+        Resource = "*"
+        Condition = {
+          StringLike = {
+            "aws:ResourceTag/efs.csi.aws.com/cluster" = "true"
+          }
+        }
+      },
+      {
+        Effect = "Allow"
+        Action = "elasticfilesystem:DeleteAccessPoint"
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "aws:ResourceTag/efs.csi.aws.com/cluster" = "true"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "efs_csi_policy_attachment" {
+  policy_arn = aws_iam_policy.efs_csi_policy.arn
+  role       = aws_iam_role.efs_csi_role.name
+}
